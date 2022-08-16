dd56c041d42fb23c219cbe78a5902a284f53ea9c
nikachaduneli <nikoloz.chaduneli.1@btu.edu.ge>
2022-08-17 00:19:10 +0400

    create custom RangeFilter Widget

--- places/filters.py
+++ places/filters.py
@@ -1,12 +1,16 @@
 import django_filters
 from django_filters import FilterSet, RangeFilter, ModelChoiceFilter
 from .models import Place
+from django_filters.widgets import RangeWidget
 
+
+class MyRangeWidget(RangeWidget):
+    template_name = "widgets/my_range_widget.html"
 
 class PlaceFilter(FilterSet):
     city = django_filters.CharFilter(label='City', lookup_expr='icontains')
-    price = django_filters.RangeFilter(label='Price Range')
-    score = django_filters.RangeFilter(label='score Range')
+    price = django_filters.RangeFilter(label='Price Range', widget=MyRangeWidget)
+    score = django_filters.RangeFilter(label='score Range', widget=MyRangeWidget)
 
     class Meta:
         model = Place
